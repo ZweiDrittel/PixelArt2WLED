8b03dccca692eb0e75bf2a87170d1bbd62994f1c
Veit Rautmann <veit.rautmann@web.de>
2021-03-02 21:41:09 +0100

    Implemented grid of Pixels; Implemented simple Request to WLED with fixed URL and fake data

--- frontend/src/Scenery/Scenery.tsx
+++ frontend/src/Scenery/Scenery.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import Canvas from '../Canvas/canvas';
 import TextField from '@material-ui/core/TextField';
+import { Button, createStyles, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+    },
+  }),
+);
 
 const Scenery: React.FC = () => {
   const [rows, setRows] = React.useState(16);
@@ -12,8 +20,27 @@ const Scenery: React.FC = () => {
     setCols(parseInt(event.target.value));
   };
 
+  const classes = useStyles();
+
+  const sendData = () => {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "http://192.168.178.100/json/state";
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var json = JSON.parse(xmlhttp.responseText);
+          console.log(json);
+        }
+    };
+    xmlhttp.open("POST", url, true);
+    var data = JSON.stringify({"on": true,
+                "seg": {
+                  "i": [0,[255,0,0]]}});
+    xmlhttp.send(data);
+  };
+
   return (
-    <div className="Scenery">
+    <div className={classes.root}>
       <TextField 
         id="rows"
         label="Rows"
@@ -30,6 +57,7 @@ const Scenery: React.FC = () => {
         onChange={handleColsChange} />
 
       <Canvas rows={rows} cols={cols} />
+      <Button onClick={sendData} >Upload to WLED</Button>
     </div>
   );
 }
